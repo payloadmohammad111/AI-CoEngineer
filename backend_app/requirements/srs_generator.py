43708655af8897c5edcadaa5b6da06dfae34e81f
payloadmohammad111 <221110956@psu.edu.sa>
2025-09-24 02:21:48 -0700

    Add srs_generator module for requirements

---
+++ backend_app/requirements/srs_generator.py
@@ -0,0 +1,20 @@
+"""
+srs_generator.py
+
+Provides functionality to generate Software Requirements Specification (SRS) documents.
+This module will assemble an SRS from parsed project files and extracted requirements.
+"""
+
+from typing import List
+
+
+def generate_srs(requirements: List[str], non_functional: List[str]) -> str:
+    """
+    Create an SRS document as a string given lists of requirements.
+
+    :param requirements: Functional requirements.
+    :param non_functional: Non-functional requirements.
+    :return: Generated SRS as a string.
+    """
+    # TODO: Implement SRS generation logic
+    return ""
